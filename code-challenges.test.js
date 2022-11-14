f0dfbb69b521d62567760c7f14bb944f115e613d
cibrown94 <cianibrown94@aol.com>
2022-11-14 07:44:10 -0800

    1st pass

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,18 +1,15 @@
-// ASSESSMENT 3: Coding Practical Questions with Jest
-
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
-
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
-
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
-
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("number", () => {
+  it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
+      const fibLength1 = 6
+      const fibLength2 = 10
+    expect(number(fibLength1)).map("1, 1, 2, 3, 5, 8")
+    expect(number(fibLength2)).map("1, 1, 2, 3, 5, 8, 13, 21, 34, 55")
+  })
+})
 
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -22,10 +19,68 @@ const fibLength2 = 10
 
 // b) Create the function that makes the test pass.
 
+// const number =(array) =>{
+//     if(array.number > 2){
+//         return number-1 + number -2
+//     }
+    
+// }
+
+const number =(array) =>{
+for (let i = 1; i <= array.length; i++)
+return array =>{
+}
+}
+
+// Create a function that takes in a number as an arguement greater than 2 and returns an array that length containing the numbers of the Fibonacci sequence
+// Create a variable called number that takes in number
+// Use dot notation map
+// Use length built in method to take in the 
+// // I googled the fibonacci sequence and found on javascript how to input the logicalExpression. When doing this problem, thats the only that occured. 
+// The problem wasnt the function its in the test
+// I was truly confused on the question until i googled what that actually is
+
+TypeError: expect(...).indexOf is not a function. This is the
+
+FAIL  ./code-challenges.test.js
+number
+  ✕ returns an array that length containing the numbers of the Fibonacci sequence (1 ms)
+
+● number › returns an array that length containing the numbers of the Fibonacci sequence
+
+  TypeError: expect(...).indexOf is not a function
+
+    21 |         const fibLength1 = 6
+    22 |         const fibLength2 = 10
+  > 23 |       expect(number(fibLength1)).indexOf("1, 1, 2, 3, 5, 8")
+       |                                  ^
+    24 |       expect(number(fibLength2)).indexOf("1, 1, 2, 3, 5, 8, 13, 21, 34, 55")
+    25 |     })
+    26 |   })
+
+    at Object.indexOf (code-challenges.test.js:23:34)
+
+Test Suites: 1 failed, 1 total
+Tests:       1 failed, 1 total
+Snapshots:   0 total
+Time:        0.343 s, estimated 1 s
+Ran all test suites.
+error Command failed with exit code 1.
+info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
+
+
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
+
+// Create a function that takes takes sort the objects values from least to greatest. 
+// Create a new variable that takes in the object
+// Use the .sort to sort the values in the array
+// Return the array with just the values sorted from least the greatest. 
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -49,7 +104,61 @@ const studyMinutesWeek2 = {
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
+const material = studyMinutesWeek1.sort((value)) =>{
+return value.map
+}
+ 
+// I keep gettin synthax error. All around the same as this one. 
+// I used the objects part of the syllabus and google, and the reference provided above. 
+// SyntaxError: /Users/learnacademy/golf/weeklyassessments/week-3-assessment-cibrown94/code-challenges.test.js: 'return' outside of function. (35:0)
+
+// 33 |
+// 34 | const material = studyMinutesWeek1.filter((value))
+// > 35 | return value.map
+//    | ^
+// 36 |
+// 37 |  
+// 38 |   
+
+
+
 // b) Create the function that makes the test pass.
+
+describe("object", () => {
+      it("returns an array of the values sorted from least to greatest.", () => {
+          const studyMinutesWeek1 = 
+            {sunday: '90',
+            monday: '30',
+            tuesday: '20',
+            wednesday: '15',
+            thursday: '30',
+            friday: '15',
+            saturday: '60'}
+          const studyMinutesWeek2 = 
+          {sunday: '100',
+            monday: '10',
+            tuesday: '45',
+            wednesday: '60',
+            thursday: '20',
+            friday: '15',
+            saturday: '65'}
+        expect(studyMinutesWeek1(value)).map([15, 15, 20, 30, 30, 60, 90]) 
+        expect(studyMinutesWeek2(value)).map([10, 15, 20, 45, 60, 65, 100])
+      })
+    })
+  
+    ReferenceError: value is not defined
+
+
+    const expected = { name:'component name' }
+    const actual = { name: 'component name', type: 'form' }
+    expect(actual).toMatchObject(expected)
+
+    ReferenceError: value is not defined
+This is the format i used for jest and objectTypeSpreadProperty. 
+
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
@@ -65,3 +174,34 @@ const accountTransactions3 = []
 // Expected output: []
 
 // b) Create the function that makes the test pass.
+
+describe("account", () => {
+  it("an array and returns an array of the accumulating sum. An empty array should return an empty array", () => {
+    const accountTransactions1 = [100, -17, -23, -9]
+    const accountTransactions2 = [250, -89, 100, -96]
+    const accountTransactions3 = []
+    expect(account(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(account(accountTransactions2)).toEqual([250, -89, 100, -96])
+    expect(account(accountTransactions3)).toEqual([])
+  })
+})
+
+const accounts = (array) => array.map((sum => value => sum += value)(0))
+
+const accounts =(array) =>{
+accountTransactions1.array.forEach(num, index => {
+  if(index === 0){
+    out [index] = num;
+  } else{
+    output[index]= num + output[index -1]
+  }
+return array
+  }
+  
+// Create a function, that takes in a array,
+// create a new variable called accounts and return the first index of the number subtracted by the second index and so on
+// Use the .map to return a new array
+// create logic that access every items in the index
+// I took me a second to understand the question itself
+
+https://www.tutorialspoint.com/converting-array-of-numbers-to-cumulative-sum-array-in-javascript
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,58 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is Javascript markdown that behaves like a HTML. In simple terms its the meshing of HTML and Javascript in react. 
 
-Researched answer:
+Researched answer: JSX is a syntax extension of Javascript in React. JSX makes it easier to add HTML in React. One difference between HTML and JSX is that JSX needs a return for the output. 
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager for Javascript that contains code being shared by other developers. It is a platform to share code, and install jest. It has thousands of node modules to assist developers. 
 
-Researched answer:
+Researched answer: Yarn, the acronym meaning Yet Another Resource Negiotator. Yarn is an extension system for running big information applications. It is a package manager for Javascript that allows developers and share code in one platform. 
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function in Javascript is a function that does not have anything declared in the function. 
 
-Researched answer:
+Researched answer: An anonymous function is a function that does not have any name associated with it. Its a function that is declared without any identifier.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: React state is a variable that holds information, props is an object, it is used to pass the information from one component to another. 
 
-Researched answer:
+Researched answer: React state is a variable that holds information, props (properties) are used to pass arguments as attributes through the component. React doesn’t process data, and does not store algorithms. It takes in data, handles it, and displays it. The use state holds that data/information, and hold it until passed through an argument to be displayed. 
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM is Document Object Model, which is the visual representation of code. 
 
-Researched answer:
+Researched answer:  The DOM (Document Object Model) represents the webpage as a structure. Makes it so that you can actively interact with one node instead of refreshing the screen after every user interaction. 
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: The div and span tags are not semantic elements since they provide no context as to what is inside of those tags. The difference between the two is that div is used for organization and span is an inline container. 
 
-Researched answer:
+Researched answer: The difference between the div and the span element is that the span tag is used to group elements and content inline, for styling purposes. Div is used for organizing the webpage. 
 
 ## Looking Ahead: Terms for Next Week
 
 1. Object-oriented programming:
 
+Is a computer programming model that organizes software design around data, or objects, rather than functions and logic. 
+
 2. Ruby:
+
+Is a dynamic, open source programming language with a focus on simplicity and productivity. Mainly used to build web applications and programming projects.
 
 3. Implicit return:
 
+Is a function that returns values without using the return keyword. 
+
 4. Ruby blocks:
 
+Are little anonymous functions that can be passed into methods.
+
 5. Ruby hashes:
+
+Maps each of its unique keys to a specific value. Has certain similarities to an array. 
